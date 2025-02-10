fc931fe853a267ade716cf2c6a7110a0eb39cb0b
a25612 <a25612@svalero.com>
2025-02-10 10:12:31 +0100

    empezamos la home

--- front-atemtia/src/App.vue
+++ front-atemtia/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Header from "./components/Header.vue";
+import Home from "./components/Home.vue";
 </script>
 
 <template>
     <Header />
+
+    <Home />
 </template>
 
 <style lang="scss">
---
+++ front-atemtia/src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="home">
+      <div class="bienvenida">
+        <div class="bienvenida-content">
+          <h1>Bienvenido a la App de ATEMTIA</h1>
+          <p>La plataforma de gestión de reservas diseñada para facilitar el acceso a los servicios.</p>
+        </div>
+      </div>
+      <button class="boton-app">
+         Visita la app
+      </button>
+    </div>
+  </template>
+  
+  <style lang="scss" scoped>
+  @import '@/assets/styles/variables.scss';
+  
+  .home {
+    text-align: center;
+  }
+  
+  .bienvenida {
+    color: $color-titulos;
+    padding: 50px 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .bienvenida-content h1 {
+    font-size: 2.5rem;
+    margin-bottom: 15px;
+  }
+  .boton-app {
+    background-color: $color-boton;
+    color: $color-fondo;
+    font-size: 1.5rem;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-top: 20px;
+  }
+  </style>
+  
