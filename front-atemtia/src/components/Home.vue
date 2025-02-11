4bac450758936403bf3b659253b76a8aa16047df
javierplo12 <javierplo841@gmail.com>
2025-02-11 12:52:04 +0100

    Cambio de la pagina principal

--- front-atemtia/src/components/Home.vue
+++ front-atemtia/src/components/Home.vue
@@ -1,49 +1,48 @@
 <template>
-    <div class="home">
-      <div class="bienvenida">
-        <div class="bienvenida-content">
-          <h1>Bienvenido a la App de ATEMTIA</h1>
-          <p>La plataforma de gestión de reservas diseñada para facilitar el acceso a los tutores.</p>
-        </div>
+  <div class="home">
+    <div class="bienvenida">
+      <div class="bienvenida-content">
+        <h1>Bienvenido a la App de ATEMTIA</h1>
+        <p>La plataforma de gestión de reservas diseñada para facilitar el acceso a los tutores.</p>
       </div>
+    </div>
 
-      <router-link  class="boton-app" to="/login">Visita la app</router-link>
+    <router-link class="boton-app" to="/login">Visita la app</router-link>
 
-      <section class="info">
+    <section class="info-container">
+      <div class="info">
         <h2>¿Qué ofrecemos?</h2>
         <p>
           ATEMTIA App es una aplicación web que permite a los tutores gestionar sus reservas e incidencias 
           de forma sencilla y rápida.
         </p>
-      </section>
-  
-      <section class="info">
+      </div>
+
+      <div class="info">
         <h2>¿Qué puedes hacer?</h2>
         <p>
           Dentro de la app podrás cancelar, mover o hacer reservas de nuestros servicios. 
           También podrás comunicarte con los profesionales.
         </p>
-      </section>
-  
-      <section class="info">
+      </div>
+
+      <div class="info">
         <h2>Más información sobre Atemtia</h2>
         <p>
           Si quieres saber más información sobre Atemtia y sus servicios, visita nuestra página web.
         </p>
-        <button 
-          class="boton-web-atemtia" 
-          @click="redirigirAWeb"
-        >
+        <button class="boton-web-atemtia" @click="redirigirAWeb">
           Más información
         </button>
-      </section>
-      <button class="boton-scroll-top" @click="scrollToTop">↑</button>
+      </div>
+    </section>
 
-    </div>
-  </template>
-  
-  <script>
- export default {
+    <button class="boton-scroll-top" @click="scrollToTop">↑</button>
+  </div>
+</template>
+
+<script>
+export default {
   methods: {
     redirigirAWeb() {
       window.location.href = "https://espacioatemtia.es/";
@@ -51,45 +50,44 @@
     scrollToTop() {
       window.scrollTo({
         top: 0,
-        behavior: "smooth" // Hace que la transición sea suave
+        behavior: "smooth" 
       });
     }
   }
 };
+</script>
 
-  
-  </script>
-  
-  <style lang="scss" scoped>
-  @import '../assets/styles/variables.scss';
-  .home {
-    text-align: center;
+<style lang="scss" scoped>
+@import '../assets/styles/variables.scss';
+
+.home {
+  text-align: center;
+}
+
+.bienvenida {
+  color: $color-titulos;
+  padding: 45px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .bienvenida-content h1 {
+    font-size: 26px;
   }
-  
-  .bienvenida {
-    color: $color-titulos;
-    padding: 45px 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  
-    .bienvenida-content h1 {
-      font-size: 26px;
-    }
-  }
-  
-  button {
-    padding: 10px 15px;
-    font-size: 19px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.3s ease, color 0.3s ease, 
-                transform 0.2s ease, box-shadow 0.3s ease;
-  }
-  
-  .boton-app {
+}
+
+button {
+  padding: 10px 15px;
+  font-size: 19px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease, color 0.3s ease, 
+              transform 0.2s ease, box-shadow 0.3s ease;
+}
+
+.boton-app {
   display: inline-block;
   text-decoration: none;
   padding: 10px 15px;
@@ -126,59 +124,80 @@
   }
 }
 
-  
-  .boton-web-atemtia {
-    margin-top: 20px;
-    background-color: $color-boton;
-    color: $color-fondo;
-    position: relative;
-    overflow: hidden;
-  
-    &:hover {
-      background-color: $color-secundario;
-      transform: scale(1.05);
-      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-    }
-  
-    &::after {
-      content: "";
-      position: absolute;
-      top: 0;
-      left: -100%;
-      width: 100%;
-      height: 100%;
-      background: rgba(255, 255, 255, 0.2);
-      transition: left 0.4s ease-in-out;
-    }
-  
-    &:hover::after {
-      left: 100%;
-    }
+.boton-web-atemtia {
+  margin-top: 20px;
+  background-color: $color-boton;
+  color: $color-fondo;
+  position: relative;
+  overflow: hidden;
+
+  &:hover {
+    background-color: $color-secundario;
+    transform: scale(1.05);
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
   }
-  
-  .info {
-    background-color: $color-principal;
-    color: $color-fondo;
-    padding: 40px 20px;
-    margin-top: 20px;
-    text-align: center;
-  
-    h2 {
-      font-size: 24px;
-      color: $color-fondo;
-      margin-bottom: 12px;
-      font-family: $fuente-principal;
-    }
-  
-    p {
-      font-size: 18px;
-      font-family: $fuente-principal;
-      line-height: 1.5;
-      max-width: 800px;
-      margin: 0 auto;
-    }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: -100%;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.2);
+    transition: left 0.4s ease-in-out;
   }
-  .boton-scroll-top {
+
+  &:hover::after {
+    left: 100%;
+  }
+}
+
+
+.info-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+
+  @media (min-width: 1024px) {
+    flex-direction: row;
+    justify-content: center;
+    align-items: stretch;
+  }
+}
+
+
+.info {
+  flex: 1;
+  background-color: $color-principal;
+  color: $color-fondo;
+  padding: 40px 20px;
+  text-align: center;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+
+  h2 {
+    font-size: 24px;
+    margin-bottom: 12px;
+    font-family: $fuente-principal;
+  }
+
+  p {
+    font-size: 18px;
+    font-family: $fuente-principal;
+    line-height: 1.5;
+    max-width: 800px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 1024px) {
+    padding: 40px;
+    max-width: 33%;
+  }
+}
+
+.boton-scroll-top {
   position: fixed;
   bottom: 20px;
   right: 20px;
@@ -198,6 +217,4 @@
     transform: scale(1.1);
   }
 }
-
-  </style>
-  
+</style>
