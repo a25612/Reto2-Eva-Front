d16c58769be7c97c810f83fcc63491de8e27839f
javierplo12 <javierplo841@gmail.com>
2025-02-25 23:28:27 +0100

    Medyquery de carrusel

--- front-atemtia/src/components/Carrusel.vue
+++ front-atemtia/src/components/Carrusel.vue
@@ -36,6 +36,24 @@ onMounted(() => {
           <p class="carrusel-autor">Ruben Arnadillo</p>
         </div>
       </div>
+
+      <div class="carrusel-tarjeta">
+        <img src="https://espacioatemtia.es/wp-content/uploads/2023/07/Piscina-Atemtia-Terapias-Acu%C3%A1ticas2-scaled-1280x852.jpg" class="carrusel-imagen">
+        <div class="carrusel-content">
+          <h3 class="carrusel-titulo">Curso 4</h3>
+          <p class="carrusel-fecha">13 de febrero de 2025</p>
+          <p class="carrusel-autor">Javier Plo</p>
+        </div>
+      </div>
+
+      <div class="carrusel-tarjeta">
+        <img src="https://espacioatemtia.es/wp-content/uploads/2023/07/Piscina-Atemtia-Terapias-Acu%C3%A1ticas2-scaled-1280x852.jpg" class="carrusel-imagen">
+        <div class="carrusel-content">
+          <h3 class="carrusel-titulo">Curso 5</h3>
+          <p class="carrusel-fecha">14 de febrero de 2025</p>
+          <p class="carrusel-autor">Ruben Arnadillo</p>
+        </div>
+      </div>
     </div>
 
     <!-- SCROLL PARA EL CARRUSEL -->
@@ -46,80 +64,101 @@ onMounted(() => {
 </template>
 
 <style lang="scss">
-    @import '../assets/styles/variables.scss';
-    .carrusel {
-    margin-top: -95px;
-    overflow: hidden;
-    padding: 16px;
-    margin-left: 40px;
-    padding: 90px 10px 0;
-  
-    &-container {
-      display: flex;
-      overflow-x: auto;
-      scroll-snap-type: x mandatory;
-      gap: 16px;
-      padding-bottom: 16px;
-      scroll-behavior: smooth;
-      align-items: center;
-  
-      &::-webkit-scrollbar {
-        display: none;
-      }
-    }
-  
-    &-tarjeta {
-      flex: 0 0 85%; 
-      background-color: $color-fondo;
-      border-radius: 8px;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-      display: flex;
-      flex-direction: column;
-    }
-  
-    &-imagen {
-      width: 100%;
-      height: 120px;
-      object-fit: cover;
-    }
-  
-    &-content {
-      padding: 12px;
-  
-      .carrusel-titulo {
-        font-size: 16px;
-        color: #333;
-        margin: 0 0 8px;
-      }
-  
-      .carrusel-fecha {
-        font-size: 14px;
-        color: #666;
-      }
+ @import '../assets/styles/variables.scss';
 
-      .carrusel-autor {
-        font-size: 14px;
-        color: #666;
-      }
+.carrusel {
+  margin-top: -95px;
+  overflow: hidden;
+  padding: 16px;
+  margin-left: 40px;
+  padding: 90px 10px 0;
+
+  &-container {
+    display: flex;
+    overflow-x: auto;
+    scroll-snap-type: x mandatory;
+    gap: 16px;
+    padding-bottom: 16px;
+    scroll-behavior: smooth;
+    align-items: center;
+
+    &::-webkit-scrollbar {
+      display: none;
     }
-  
-    &-scroll {
-      position: relative;
-      width: 100%;
-      height: 8px;
-      background-color: #EEEEEE;
-      border-radius: 4px;
-      margin-top: 8px;
-      overflow: hidden;
-      cursor: pointer;
-    }
-       &-scrollbar {
-        height: 100%;
-        width: 25%; 
-        background-color: $color-boton;
-        border-radius: 4px;
-      }
-    
   }
+
+  &-tarjeta {
+    flex: 0 0 85%; 
+    background-color: $color-fondo;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+  }
+
+  &-imagen {
+    width: 100%;
+    height: 120px;
+    object-fit: cover;
+  }
+
+  &-content {
+    padding: 12px;
+
+    .carrusel-titulo {
+      font-size: 16px;
+      color: #333;
+      margin: 0 0 8px;
+    }
+
+    .carrusel-fecha,
+    .carrusel-autor {
+      font-size: 14px;
+      color: #666;
+    }
+  }
+
+  &-scroll {
+    position: relative;
+    width: 100%;
+    height: 8px;
+    background-color: #EEEEEE;
+    border-radius: 4px;
+    margin-top: 8px;
+    overflow: hidden;
+    cursor: pointer;
+  }
+
+  &-scrollbar {
+    height: 100%;
+    width: 25%; 
+    background-color: $color-boton;
+    border-radius: 4px;
+  }
+
+  @media (min-width: 1024px) { //Iniciamos la media query para pantallas de 1024px 
+  .carrusel-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); 
+    gap: 30px; 
+    justify-content: center; 
+    align-items: center; 
+    overflow-x: visible;
+    padding-bottom: 0;
+  }
+
+  .carrusel-tarjeta {
+    flex: initial;
+    width: 100%;
+    height: auto; 
+    margin: 0 auto; 
+  }
+
+  .carrusel-scroll {
+    display: none; 
+  }
+}
+}
+
 </style>
 
